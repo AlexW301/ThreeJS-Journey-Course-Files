18b6867fe70250a3b94ff8dd482bb29bb985e657
Alex Waller <alex@pmlhomeloans.com>
2022-05-11 13:04:20 -0400

    lesson 26 code structuring for bigger projects

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -6,6 +6,7 @@ import Renderer from "./Renderer"
 import World from "./World/World"
 import Resources from "./Utils/Resources"
 import sources from "./sources.js"
+import Debug from "./Utils/Debug"
 
 let instance = null
 
@@ -26,6 +27,7 @@ export default class Experience
         this.canvas = canvas
         
         // Setup
+        this.debug = new Debug()
         this.sizes = new Sizes(canvas)
         this.time = new Time()
         this.scene = new THREE.Scene()
@@ -56,6 +58,40 @@ export default class Experience
     update()
     {
         this.camera.update()
+        this.world.update()
         this.renderer.update()
+    }
+
+    destroy()
+    {
+        this.sizes.off('resize')
+        this.time.off('tick')
+
+        // Traverse the whole scene
+        this.scene.traverse((child) =>
+        {
+            if(child instanceof THREE.Mesh)
+            {
+                child.geometry.dispose()
+
+                for(const key in child.material)
+                {
+                    const value = child.material[key]
+
+                    if(value && typeof value.dispose === 'function')
+                    {
+                        value.dispose()
+                    }
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
+
+        if(this.debug.active)
+        {
+            this.debug.ui.destroy()
+        }
     }
 }
--- 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -3,20 +3,23 @@ import Environment from "./Enviroment";
 import Floor from "./Floor";
 import Fox from "./Fox";
 
-export default class World
-{
-    constructor()
-    {
-        this.experience = new Experience()
-        this.scene = this.experience.scene
-        this.resources = this.experience.resources
+export default class World {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
 
-        this.resources.on('ready', () =>
-        {
-            // Setup
-            this.floor = new Floor()
-            this.fox = new Fox()
-            this.enviroment = new Environment()
-        })
+    this.resources.on("ready", () => {
+      // Setup
+      this.floor = new Floor();
+      this.fox = new Fox();
+      this.enviroment = new Environment();
+    });
+  }
+
+  update() {
+    if (this.fox) {
+      this.fox.update();
     }
+  }
 }
