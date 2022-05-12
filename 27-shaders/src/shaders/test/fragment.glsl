8aee792db7f9a438b8ef8bb2ab076037de69eb70
Alex Waller <alex@pmlhomeloans.com>
2022-05-12 13:37:35 -0400

    lesson 27 shaders

--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -1,5 +1,15 @@
-precision mediump float;
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
+
+// varying float vRandom;
+
 void main()
 {
-    gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0);
+    vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 2.0 + 0.8;
+    // gl_FragColor = vec4(0.5, vRandom * 0.5, 1.0, 1.0);
+    gl_FragColor = textureColor;
 }
